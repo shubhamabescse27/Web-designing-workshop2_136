ea8d4b55d5be7557783998401a627c622d81a53e
shubham jaiswal <sj430149@gmail.com>
2026-04-07 01:34:45 +0530

    let js

--- BOOTSTRAP/bootstrap using download/javaScript/js variable/var.js
+++ BOOTSTRAP/bootstrap using download/javaScript/js variable/var.js
@@ -4,12 +4,18 @@
 //const
 
 //var
-var a = 10;
-console.log(a); //10
-var a = 20;
-console.log(a); //20
+// var a = 10;
+// console.log(a);
+//var a = 20;
+//console.log(a);
 
-if (true) {
-    var b = 30;
-}
-console.log(b); //30
+//if (true) {
+// var b = 30;
+//}
+//console.log(b); 
+//let
+//const
+let marks = 90;
+console.log(marks);
+let marks = 95; // syntax error : identifier 'marks' has
+console.log(marks);
