2eb057818ff1fcae1640f77fd35c809e8d384849
shubham jaiswal <sj430149@gmail.com>
2026-04-11 19:30:16 +0530

    full data type function array loop for each loop

--- BOOTSTRAP/bootstrap using download/javaScript/Datatype/app.js
+++ BOOTSTRAP/bootstrap using download/javaScript/Datatype/app.js
@@ -59,4 +59,63 @@ console.log(arrnew); //[1,2,3,4]
 //pop() - removes the lats element from the array
 arrnew.pop();
 console.log(arrnew); //[1,2,3]
+//shift( ) - removes the first element from the array
+arrnew.shift();
 
+console.log(arrnew); //[2,3]
+//unshift() - adds an element to the beginning of the array
+arrnew.unshift(5);
+console.log(arrnew);
+//for loop on array
+for (let i = 0; i< arrnew.length; i++) {
+    console.log(arrnew[i]);
+}
+// for each loop on array // forEach() method executes a provided function
+
+arrnew.forEach (function(element){
+    console.log(element);
+
+});
+//map() method creates a new array populated with the results of calling array
+let squareArr = arrnew.map(function(element) {
+    return element * element;
+});
+console.log(squareArr); //[25 , 4 ,9]
+//filter() method creates a new array with all elements that pas the test impleted by the provide unction
+let evenArr = arrnew.filter(function(element){
+    return element % 2 === 0;
+});
+console.log(evenArr); //[2]
+
+//reduce() method executes a reducer function (that you provided) on each
+let sum = arrnew.reduce(function(accumulator , currentValue) {
+return accumulator + currentValue;
+} , 0);
+console.log(sum); //10
+//function //reusable block of code that performs a specific task
+function greet() {
+    console.log("hello, world!");
+}
+    greet(); //Hello , world
+    //function with parameters
+    function greet(name) {
+        console.log("Hello, " + name + "!");
+    }
+    greet("Alice"); //Hello, Alice!
+    //function with return value
+function add(a,b) {
+  return a + b;  
+}
+let result = add(5, 10);
+console.log(result); //15
+//function expression can also be defined as an expression and sorted in a variable
+let greet2 = function(name) {
+    console.log("Hi, " + name + "!");
+};
+greet2("bob");//Hi, Bob!
+//template literals // allows embedded expressions and multi_line strings
+let name3 = "Charlie";
+let greeting = `Hello, ${name3}!` ;
+let greeting2 =    `Hi, ${name3}`;
+console.log(greeting); //Hello Charlie!
+console.log(greeting2); //Hi , Charlie!
